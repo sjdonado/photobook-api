2e837ca163468b201e69ad9786f34d3a75bcd563
Gustavo Morales <gustavo.morales@gmail.com>
2018-06-30 11:23:57 -0500

    Manage routes not found and internal server error

--- server/index.js
+++ server/index.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
+app.get('/', (req, res, next) => {
+  res.json({
+    message: 'Welcome to API',
+  });
+});
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({
+    message: 'Not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500);
+  res.json({
+    message: 'Server error',
+  });
 });
 
 module.exports = app;
